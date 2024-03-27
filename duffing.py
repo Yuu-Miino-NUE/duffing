15e8afd99f3ff15545f3f69bb919fc8d01022967
Yuu Miino <ymiino@naruto-u.ac.jp>
2024-03-27 16:59:10 +0900

    Initialization

---
+++ duffing.py
@@ -0,0 +1,91 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+
+
+class Parameter:
+    def __init__(self, k: float = 0.2, B: float = 0.1, B0: float = 0.1) -> None:
+        self.k = k
+        self.B = B
+        self.B0 = B0
+
+
+def ode_func(t, vec_x, param: Parameter):
+    x, y = vec_x
+    k, B, B0 = param.k, param.B, param.B0
+
+    dxdt = y
+    dydt = -k * y - x**3 + B0 + B * np.cos(t)
+
+    return np.array([dxdt, dydt])
+
+
+def ode_func_jac_x(t, vec_x, param: Parameter):
+    x = vec_x[0]
+    k = param.k
+
+    return np.array([[0, 1], [-3 * x**2, -k]])
+
+
+def ode_func_jac_p(t, vec_x, param: Parameter):
+    y = vec_x[1]
+
+    return {
+        "k": np.array([0, -y]),
+        "B0": np.array([0, 1]),
+        "B": np.array([0, np.cos(t)]),
+    }
+
+
+def ode_func_calc_jac(t, vec_x, param: Parameter, param_key: str | None = None):
+    ret = ode_func_jac_x(t, vec_x[0:2], param) @ vec_x[2:].reshape(2, 3, order="F")
+
+    if param_key is not None:
+        ret[:, 2] += ode_func_jac_p(t, vec_x[0:2], param)[param_key]
+
+    return ret.flatten(order="F")
+
+
+base_period = 2 * np.pi
+
+
+def poincare_map(
+    vec_x,
+    param: Parameter,
+    itr_cnt: int = 1,
+    calc_jac: bool = False,
+    pjac_key: str | None = None,
+):
+    # Setup
+    if not calc_jac:
+        func = lambda _t, _x: ode_func(_t, _x, param)
+        x0 = vec_x.copy()
+    else:
+        func = lambda _t, _x: np.hstack(
+            [ode_func(_t, _x[0:2], param), ode_func_calc_jac(_t, _x, param, pjac_key)]
+        ).flatten()
+        x0 = np.hstack([vec_x, np.eye(2).flatten(), np.zeros(2)])
+        jac = np.eye(2)
+        jac_p = np.zeros(2)
+
+    # Calculate
+    for _ in range(itr_cnt):
+        sol = solve_ivp(func, [0, base_period], x0, rtol=1e-8)
+
+        x0[:2] = sol.y[:2, -1]
+        if calc_jac:
+            jack = sol.y[2:6, -1].reshape(2, 2, order="F")
+            jac_p = jack @ jac_p + sol.y[6:8, -1]
+            jac = jack @ jac
+
+    # Return
+    ret = {"x": np.array(x0[:2])}
+    if calc_jac:
+        ret["jac"] = jac
+        ret[f"jac_p({pjac_key})"] = jac_p
+
+    return ret
+
+
+poincare_map(
+    [0.23252517, 0.07522187], Parameter(), itr_cnt=2, calc_jac=True, pjac_key="B"
+)
